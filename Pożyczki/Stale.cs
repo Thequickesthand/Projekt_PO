ca18fcf6f3e817cb56e1b4204121c0c301ca5389
Damian Cichy <lubiezielonebanany@gmail.com>
2019-12-23 23:16:54 +0100

    Dodanie statycznej klasy "Stale"

---
+++ Pożyczki/Stale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pożyczki
+{
+    public static class Stale
+    {
+        public static double oprocentowanie = 0.08;
+        public static double op_dlugu = 0.15;
+        public static double szypko = 0.30;
+        public static double wolmo = 0.10;
+    }
+}

392cab7ee013b9223a098a015fe78264e85ff4de
Damian Cichy <lubiezielonebanany@gmail.com>
2019-12-24 01:31:09 +0100

    Dodałem klase Klient, Dluznik i interfejs IWyswietl.

---
+++ Pożyczki/IWyswietl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pożyczki
+{
+    interface IWyswietl
+    {
+        public void Obecni(Klient klient);
+        public void Byli(Klient klient);
+        public void Wszyscy(Klient klient);
+    }
+}
---
+++ Pożyczki/Klient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pożyczki
+{
+    class Klient : IWyswietl
+    {
+        public void Byli(Klient klient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Obecni(Klient klient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Wszyscy(Klient klient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
